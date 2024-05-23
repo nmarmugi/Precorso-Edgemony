68ced0458842f1edf99f02d6d2f966d36df2012f
Nicola Marmugi <marmu1@hotmail.it>
2024-05-23 14:34:01 +0200

    inizio seconda settimana js

---
+++ Corso/JS/Esercitazione_23-05-24/script.js
@@ -0,0 +1,29 @@
+const headingEl = document.body.querySelector('#heading');
+console.log(headingEl);
+
+const buttonEls = document.body.querySelectorAll('button');
+for(let i = 0; i < buttonEls.length; i++) {
+	console.log(buttonEls[i].innerHTML);
+}
+
+const resetBtn = document.body.querySelectorAll('button')[0];
+const clickBtn = document.body.querySelectorAll('button')[1];
+clickBtn.onclick = function() {
+	let clickBtnToNumber = Number(clickBtn.innerHTML);
+	if (isNaN(clickBtnToNumber)) {
+		clickBtn.innerHTML = 0;
+		console.log(clickBtn.innerHTML);
+	}
+	if (!isNaN(clickBtnToNumber)) {
+		clickBtn.innerHTML++;
+		console.log(clickBtn.innerHTML);
+	}
+}
+
+resetBtn.onclick = function() {
+	let clickBtnToNumber = Number(clickBtn.innerHTML);
+	if (isNaN(clickBtnToNumber)) {
+		return;
+	}
+	clickBtn.innerHTML = 'Click me!';
+}
