9661bf76570b7b7ce3f6dd12e5b989971fefaba0
Nicola Marmugi <marmu1@hotmail.it>
2024-05-25 02:38:06 +0200

    fine seconda settimana js

--- Corso/JS/Esercitazione_24-05-24/script.js
+++ Corso/JS/Esercitazione_24-05-24/script.js
@@ -1,23 +1,48 @@
 const h1El = document.createElement('h1');
-const pEl = document.createElement('p');
+h1El.innerHTML = ('Sono un titolo h1');
 document.body.insertAdjacentElement('afterbegin', h1El);
+
+const pEl = document.createElement('p');
+pEl.innerHTML = ('Sono un paragrafo');
 document.body.append(pEl);
 
-const buttonEl = document.createElement('button');
-pEl.insertAdjacentElement('afterend', buttonEl);
-buttonEl.innerHTML = 0;
+const divEl = document.createElement('div');
+pEl.insertAdjacentElement('afterend', divEl);
 
+const buttonEl = document.createElement('button');
+buttonEl.innerHTML = 0;
+divEl.insertAdjacentElement('beforeend', buttonEl);
+
+divEl.style.display = 'flex';
+divEl.style.justifyContent = 'center';
+divEl.style.height = '50vh';
+divEl.style.alignItems = 'center';
 
 buttonEl.style.backgroundColor = 'green';
 buttonEl.style.borderRadius = '50%';
 buttonEl.style.border = 'none';
 buttonEl.style.fontSize = '30px';
-buttonEl.style.padding = '0 8px';
+buttonEl.style.width = '50px';
+buttonEl.style.height = '50px';
 
 
 buttonEl.onmouseenter = function(){
 	buttonEl.style.backgroundColor = "red";
 	buttonEl.style.color = 'black';
+	onclick = function(){
+		buttonEl.innerHTML++;
+		const buttonWidth = parseInt(buttonEl.style.width);
+		const newButtonWidth = buttonWidth + 5;
+		buttonEl.style.width = newButtonWidth + 'px';
+		const buttonHeight = parseInt(buttonEl.style.height);
+		const newButtonHeight = buttonHeight + 5;
+		buttonEl.style.height = newButtonHeight + 'px';
+		if (buttonEl.innerHTML === '50') {
+			buttonEl.innerHTML = 0;
+			buttonEl.style.width = '50px';
+			buttonEl.style.height = '50px';
+		}
+	}
 }
 
 buttonEl.onmouseout = function(){
@@ -25,64 +50,33 @@ buttonEl.onmouseout = function(){
 	buttonEl.style.color = 'white';
 	if (Number(buttonEl.innerHTML) === 0) {
 		buttonEl.innerHTML = 0;
-		buttonEl.style.padding = '0 8px';
 		return;
 	}
 	buttonEl.innerHTML--;
-	if (Number(buttonEl.innerHTML) === 1) {
-		buttonEl.style.padding = '0 8px';
-		return;
-	}
-	const buttonPadding = parseInt(buttonEl.style.padding);
-	const newButtonPadding = buttonPadding - 5;
-	buttonEl.style.padding = newButtonPadding + 'px';
+	const buttonWidth = parseInt(buttonEl.style.width);
+	const newButtonWidth = buttonWidth - 5;
+	buttonEl.style.width = newButtonWidth + 'px';
+	const buttonHeight = parseInt(buttonEl.style.height);
+	const newButtonHeight = buttonHeight - 5;
+	buttonEl.style.height = newButtonHeight + 'px';
 }
 
-buttonEl.onclick = function(){
-	buttonEl.innerHTML++;
-	if (buttonEl.innerHTML === '1') {
-		buttonEl.style.padding = '0 8px';
-		return;
-	}
-	if (Number(buttonEl.innerHTML) >= 2 && Number(buttonEl.innerHTML) <= 9) {
-			const buttonPaddingLeft = parseInt(buttonEl.style.paddingLeft);
-			const newButtonPaddingLeft = buttonPaddingLeft + 5;
-			buttonEl.style.paddingLeft = newButtonPaddingLeft + 'px';
-			const buttonPaddingRight = parseInt(buttonEl.style.paddingRight);
-			const newButtonPaddingRight = buttonPaddingRight + 5;
-			buttonEl.style.paddingRight = newButtonPaddingRight + 'px';
-			const buttonPaddingTop = parseInt(buttonEl.style.paddingTop);
-			const newButtonPaddingTop = buttonPaddingTop + 5;
-			buttonEl.style.paddingTop = newButtonPaddingTop + 'px';
-			const buttonPaddingBottom = parseInt(buttonEl.style.paddingBottom);
-			const newButtonPaddingBottom = buttonPaddingBottom + 5;
-			buttonEl.style.paddingBottom = newButtonPaddingBottom + 'px';
-	}
-	else {
-		const buttonPadding = parseInt(buttonEl.style.padding);
-		const newButtonPadding = buttonPadding + 5;
-		buttonEl.style.padding = newButtonPadding + 'px';
-	}
-	if (buttonEl.innerHTML === '50') {
-		buttonEl.innerHTML = 0;
-		buttonEl.style.padding = '0 8px';
-	}
-}
+
 
 
 const formEl = document.body.querySelector('form');
 const inputEl = document.body.querySelector('input');
-const ulEl = document.body.querySelector('ul');
+const ulEl = document.body.querySelector('.list');
 
 formEl.onsubmit = function(event) {
 	event.preventDefault();
 	const inputValue = inputEl.value;
 	if (inputValue && inputValue.includes(' ')) {
-		const elementEl = document.createElement('li');
-		elementEl.innerHTML = inputValue;
-		ulEl.insertAdjacentElement('beforeend', elementEl);
-		elementEl.onclick = function() {
-			console.log(elementEl.textContent);
+		const liEl = document.createElement('li');
+		liEl.innerHTML = `${inputValue}`;
+		ulEl.insertAdjacentElement('afterbegin', liEl);
+		liEl.onclick = function() {
+			console.log(liEl.innerHTML);
 		}
 	}
 }
